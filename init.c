5dc1791dcaa3c2814c229e8875593f229f0354ac
ricvrdv <costagarcia.r95@gmail.com>
2025-09-15 16:14:29 +0100

    feat: add error handling in init_game()

--- init.c
+++ init.c
@@ -4,19 +4,13 @@ void    init_game(t_game *game)
 {
     game->mlx_ptr = mlx_init();
     if (!game->mlx_ptr)
-    {
-        // Cleanup and exit
-    }
+        handle_error("Failed to initialize MinilibX.\n");
     game->win_ptr = mlx_new_window(game->mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "CUB3D");
     if (!game->win_ptr)
-    {
-        // Cleanup and exit
-    }
+        handle_error("Failed to open window.\n");
     game->img.img_ptr = mlx_new_image(game->mlx_ptr, WIN_WIDTH, WIN_HEIGHT);
     if (!game->img.img_ptr)
-    {
-        // Cleanup and exit
-    }
+        handle_error("Failed to create image buffer.\n");
     game->img.addr = mlx_get_data_addr(game->img.img_ptr,
         &game->img.bits_per_pixel, &game->img.line_length,
         &game->img.endian);
