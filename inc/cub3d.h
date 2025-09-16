fae9a01187ccae87229c0203b6b7ef69873e0358
Daniela-Padilha <ddo-carm@LAPTOP-REDJ8VVL.localdomain>
2025-09-16 14:53:20 +0100

    refactor: add t_colors struct

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -48,6 +48,14 @@ typedef struct  s_textures
     t_img   east;
 } t_textures;
 
+typedef struct s_colors
+{
+    char    id;
+    int     r_code;
+    int     g_code;
+    int     b_code;
+}   t_colors;
+
 typedef struct  s_game
 {
     void        *mlx_ptr;
@@ -55,6 +63,7 @@ typedef struct  s_game
     t_img       img;
     t_player    player;
     t_textures  textures;
+    t_colors    colors;
 } t_game;
 
 // init
