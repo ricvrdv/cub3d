63f10c22ed1391207eafbd8fd044f9687022b43d
Daniela Sofia Do Carmo Padilha <ddo-carm@c2r3s6.42porto.com>
2025-09-20 19:00:26 +0100

    chore: had to commit to save progress

--- src/texture_parser.c
+++ src/texture_parser.c
@@ -1,7 +1,31 @@
 #include "../inc/cub3d.h"
 
+static int path_valid(char *line)
+{
+	char	**split;
+	char	*last;
+	int		i;
+
+	split = ft_split(line, '.');
+	if (!split || !split[0])
+		return (-1);
+	if (ft_strncmp(split[1], "xpm", 3) != 0)
+		return (free(split), -1);
+	last = ft_strrchr(split[0], '/');
+	i = 0;
+	if (!last[i + 1])
+		return (free(split), -1);
+	free(split);
+	return (0);
+}
+
 static int	set_texture_path(t_game *game, char *line, const char *id, char **target)
 {
+	if (path_valid(line) == -1)
+	{
+		handle_error(game, "Texture path is not valid\n");
+		return (1);
+	}
 	if ((ft_strncmp(line, id, 2) == 0) && !*target)
 	{
 		line += 2;
